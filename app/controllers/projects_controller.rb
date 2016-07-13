76324d3066f9f17aaa753985296cd7f1423766c7
SEILA <seila.tupone@gmail.com>
2016-07-13 16:59:45 -0400

    Edit form new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,7 +26,7 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:name, :description, :category, :location, :goal, :deadline)
+    params.require(:project).permit(:name, :description, :category, :location, :goal, :deadline, :name, :description, :amount)
   end
 
 end
