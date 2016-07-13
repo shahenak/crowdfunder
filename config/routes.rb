e046c5744001364187b04fffe678da627437065a
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-07-13 16:33:05 -0400

    created users show page and populated with first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'projects#index'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :projects do
     resources :rewards
