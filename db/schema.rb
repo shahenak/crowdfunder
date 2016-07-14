d6b950c195e7870e6f1ac4d1f75fe2d32df02f57
Benny <bennyxnguyen@gmail.com>
2016-07-14 16:27:25 -0400

    working on index css and applications css

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160713231553) do
-
+ActiveRecord::Schema.define(version: 20160714154719) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -21,10 +19,10 @@ ActiveRecord::Schema.define(version: 20160713231553) do
     t.datetime "deadline"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "description"
     t.string   "category"
-    t.string   "location"
     t.string   "image"
+    t.string   "description"
+    t.string   "location"
   end
 
   create_table "rewards", force: :cascade do |t|
